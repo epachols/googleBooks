a778dae44576ad2a0340c229125f7f1eb2c91cfe
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-09-02 08:51:57 -0700

    successfully imported antd and css, now moving to less override

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import "./App.css"
+
+//TODO:***TEST****
+import { Button } from 'antd';
+//TODO:***TEST****
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+        <h1>Catalog - for all your kitty book needs</h1>
+        <Button type="primary">Comin' At Ya</Button>
     </div>
   );
 }
