dc2304fa30e5a3097086344514ee9bfc46711cae
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-09-02 08:22:28 -0700

    added unified topology and urlparser to mongoose connect line in server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.listen(PORT, () => {
