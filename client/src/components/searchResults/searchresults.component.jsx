a8760144e74f0bbfd29fda7ee0186f4265c50c27
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-09-02 15:55:24 -0700

    functional representation and styling in searchresults box, need to create book card next

--- client/src/components/searchResults/searchresults.component.jsx
+++ client/src/components/searchResults/searchresults.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./searchresults.css"
+import BookCart from "../images/librarian.png"
 
 export default function SearchResults(props) {
     return (
         <div className='SearchResults'>
             {/* TODO: TERNARY FOR whether or not there are results, displaying book cart if not there */}
             <h3 style={{ textAlign: "left", marginLeft: "2rem" }}>Search results</h3>
+            {props.results&&props.results[0]? "REPLACE ME WITH BOOK CARDS, YE LAND LUBBER" : <img  style={{height:"200px"}} src={BookCart}></img>}
         </div>
     )
 }
