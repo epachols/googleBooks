bc9a9485d3789ce4194743e50e58bd4c0a130c7e
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-09-04 11:38:51 -0700

    added save to mongo db function, was having trouble recognizing the parsed img object, so reduced object sent to exact keyvalue pairs

--- client/src/components/bookcard/bookcard.component.jsx
+++ client/src/components/bookcard/bookcard.component.jsx
@@ -2,20 +2,28 @@ import React from "react";
 import "./bookcard.css";
 import { Row, Col, Divider, Button, message } from "antd";
 import BookCart from "../images/librarian.png";
-import API from "../../utils/API"
+import API from "../../utils/API";
 
 export default function BookCard(props) {
+  const saveBook = () => {
+    const bookData = {
+      title: props.data.title,
+      subtitle: props.data.subtitle,
+      authors: props.data.authors,
+      description: props.data.description,
+      image: props.data.image.thumbnail,
+      link: props.data.link,
+      pageCount: props.data.pageCount,
+    };
+    console.log(bookData)
 
-  const saveBook = (data) => {
-    // console.log(data)
-    // console.log(JSON.stringify(data))
-    API.saveBook(JSON.stringify(data))
+    API.saveBook(bookData)
     .then(()=>{
-      // message.info('added this book to "saved"')
+      message.info('added this book to "saved"')
       console.log('success')
     })
     .catch((err) => console.log(err));
-  }
+  };
 
   return (
     <div className="bookCard">
@@ -24,20 +32,25 @@ export default function BookCard(props) {
           <h2>{props.data.title}</h2>
           <h5>{props.data.subtitle}</h5>
         </Col>
-        <Col span={6} style={{textAlign: "right"}}>
+        <Col span={6} style={{ textAlign: "right" }}>
           <Button>view</Button>
-          <Button onClick={()=> saveBook(props.data)}>save</Button>
+          <Button onClick={() => saveBook()}>save</Button>
         </Col>
-          <span className="Authors">{props.data.authors}</span>
+        <span className="Authors">{props.data.authors}</span>
       </Row>
       <Divider></Divider>
-      <Row gutter={16,16}>
+      <Row gutter={(16, 16)}>
         <Col span={4}>
-
-        {/* WORKING ::TODO::: pick out the library cart for OR after it is appropriately sized */}
-        <img alt="thumbnail picture" style={{width: "100%"}} src={props.data.image.smallThumbnail} />
-
-
+          {/* WORKING ::TODO::: pick out the library cart for OR after it is appropriately sized */}
+          {props.data.image && props.data.image.thumbnail ? (
+            <img
+              alt="thumbnail picture"
+              style={{ width: "100%" }}
+              src={props.data.image.thumbnail}
+            />
+          ) : (
+            <></>
+          )}
         </Col>
         <Col span={19}>{props.data.description}</Col>
       </Row>
