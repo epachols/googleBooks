f2f62e88b16e378df4990cb3c3ee6fde3ef91a96
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-09-04 08:37:13 -0700

    data in card, handled weird returns from API. investigate next expanding return limit

--- client/src/components/bookcard/bookcard.component.jsx
+++ client/src/components/bookcard/bookcard.component.jsx
@@ -7,18 +7,26 @@ export default function BookCard(props) {
     <div className="bookCard">
       <Row>
         <Col span={18}>
-          <h3>book title</h3>
-          <h5>book subtitle</h5>
-          <Divider orientation="left">Book Authors</Divider>
+          <h2>{props.data.title}</h2>
+          <h5>{props.data.subtitle}</h5>
         </Col>
-        <Col span={16}>
+        <Col span={6} style={{textAlign: "right"}}>
           <Button>view</Button>
           <Button>edit</Button>
         </Col>
+          <span className="Authors">{props.data.authors}</span>
       </Row>
-      <Row>
-        <Col span={6}>picture</Col>
-        <Col span={18}>description</Col>
+      <Divider></Divider>
+      <Row gutters={8,0}>
+        <Col span={4}>
+
+        {/* WORKING ::TODO::: pick out the library cart for OR after it is appropriately sized */}
+        {/* <img src={props.data.image.thumbnail || } /> */}
+        picture
+
+
+        </Col>
+        <Col span={19}>{props.data.description}</Col>
       </Row>
     </div>
   );
